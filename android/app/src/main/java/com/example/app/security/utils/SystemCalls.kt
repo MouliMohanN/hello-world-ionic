d061ebede420cccf37bf8ee676137c0a4ddddc3a
MouliMohanN <moulimohann@gmail.com>
2024-12-17 11:25:07 +0530

    copied code from RN, obfuscated strings

--- android/app/src/main/java/com/example/app/security/utils/SystemCalls.kt
+++ android/app/src/main/java/com/example/app/security/utils/SystemCalls.kt
@@ -1,11 +1,16 @@
 package com.example.app.security.utils
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import com.example.app.utils.SECURITY_LOG_TAG
+import com.example.app.utils.decodeToString
 
 object SystemCalls {
     fun isDetected(): Boolean {
-        val suspiciousSysCalls = arrayOf("ptrace", "mprotect")
+        // ptrace
+        val ptrace = intArrayOf(99,72,82,121,89,87,78,108)
+        // mprotect
+        val mprotect = intArrayOf(98,88,66,121,98,51,82,108,89,51,81,61)
+        val suspiciousSysCalls = arrayOf(ptrace.decodeToString(), mprotect.decodeToString())
+        println("${SECURITY_LOG_TAG.decodeToString()} - SystemCalls.isDetect - suspiciousSysCalls: ${suspiciousSysCalls.joinToString(", ")}")
         for (sysCall in suspiciousSysCalls) {
             if (isSysCallUsed(sysCall)) {
                 return true
@@ -16,15 +21,15 @@ object SystemCalls {
 
     private fun isSysCallUsed(sysCallName: String): Boolean {
         try {
-            val process = Runtime.getRuntime().exec("strace -e $sysCallName")
-            val reader = BufferedReader(InputStreamReader(process.inputStream))
-            var line: String
-            while ((reader.readLine().also { line = it }) != null) {
-                if (line.contains(sysCallName)) {
+            // strace -e
+            val strace = intArrayOf(99,51,82,121,89,87,78,108,73,67,49,108)
+            println("${SECURITY_LOG_TAG.decodeToString()} - SystemCalls.isSysCallUsed - strace: ${strace.decodeToString()}")
+            val process = Runtime.getRuntime().exec("${strace.decodeToString()} $sysCallName")
+            process.inputStream.bufferedReader().useLines { lines ->
+                if (lines.any { it.contains(sysCallName) }) {
                     return true
                 }
             }
-            reader.close()
         } catch (e: Exception) {
             e.printStackTrace()
         }
--- android/app/src/main/java/com/example/app/security/utils/hash/Manifest.kt
+++ android/app/src/main/java/com/example/app/security/utils/hash/Manifest.kt
@@ -1,6 +1,7 @@
 package com.example.app.security.utils.hash
 
 import android.content.Context
+import com.example.app.utils.SECURITY_LOG_TAG
 import com.example.app.utils.decodeToString
 import java.io.File
 import java.security.MessageDigest
@@ -14,11 +15,9 @@ object Manifest {
 
     if (manifestFile != null) {
       val manifestHash = computeFileHash(manifestFile)
-
-      println("MouliTesting - Manifest Hash: \n" +
-        "CompileTime - ${buildTimeHash} \n" +
-        "RunTime - $manifestHash \n")
-
+      println("${SECURITY_LOG_TAG.decodeToString()} - Manifest - isValid -\n" +
+              "buildTimeHash - $buildTimeHash\n" +
+              "runTimeHash - $manifestHash")
       return buildTimeHash == manifestHash
     }
     return false
@@ -26,11 +25,18 @@ object Manifest {
 
   private fun extractManifestFile(context: Context): File? {
     val apkPath = context.applicationInfo.sourceDir
-    val outputDir = context.getDir("extracted_manifest", Context.MODE_PRIVATE)
-    val manifestFile = File(outputDir, "AndroidManifest.xml")
+    // extracted_manifest
+    val extractedManifest = intArrayOf(90,88,104,48,99,109,70,106,100,71,86,107,88,50,49,104,98,109,108,109,90,88,78,48)
+    val outputDir = context.getDir(extractedManifest.decodeToString(), Context.MODE_PRIVATE)
 
+    // AndroidManifest.xml
+    val androidManifestFile = intArrayOf(81,87,53,107,99,109,57,112,90,69,49,104,98,109,108,109,90,88,78,48,76,110,104,116,98,65,61,61)
+    val manifestFile = File(outputDir, androidManifestFile.decodeToString())
+
+    println("${SECURITY_LOG_TAG.decodeToString()} - Manifest - extractManifestFile - extractedManifest: ${extractedManifest.decodeToString()}\n" +
+            "androidManifestFile - ${androidManifestFile.decodeToString()}")
     ZipFile(apkPath).use { zipFile ->
-      val entry = zipFile.getEntry("AndroidManifest.xml")
+      val entry = zipFile.getEntry(androidManifestFile.decodeToString())
       entry?.let {
         zipFile.getInputStream(it).use { input ->
           manifestFile.outputStream().use { output ->
@@ -45,7 +51,11 @@ object Manifest {
 
   private fun computeFileHash(file: File): String {
     val buffer = ByteArray(1024)
-    val digest = MessageDigest.getInstance("SHA-256")
+
+    // SHA_256
+    val SHA_256 = intArrayOf(85,48,104,66,76,84,73,49,78,103,61,61)
+    println("${SECURITY_LOG_TAG.decodeToString()} - Manifest - computeFileHash - SHA_256 - ${SHA_256.decodeToString()}")
+    val digest = MessageDigest.getInstance(SHA_256.decodeToString())
 
     file.inputStream().use { inputStream ->
       var bytesRead = inputStream.read(buffer)
