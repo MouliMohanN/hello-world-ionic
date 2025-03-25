af0a3aeb15b0e3b06078a8a15dc225e26afda83a
MouliMohanN <moulimohann@gmail.com>
2025-03-25 19:06:39 +0530

    SecurityService - Enum to String refactoring and Generic error message implementation

--- android/app/src/main/java/com/example/app/security/SecurityService.kt
+++ android/app/src/main/java/com/example/app/security/SecurityService.kt
@@ -20,63 +20,51 @@ import kotlinx.coroutines.launch
 import java.util.LinkedList
 import java.util.Queue
 
+object SecurityType {
+  // STD_01
+  val DEVELOPER_OPTIONS = intArrayOf(85, 49, 82, 69, 88, 122, 65, 120).decodeToString()
+
+  // STD_02
+  val APP_INTEGRITY = intArrayOf(85, 49, 82, 69, 88, 122, 65, 121).decodeToString()
+
+  // STD_03
+  val ROOT = intArrayOf(85, 49, 82, 69, 88, 122, 65, 122).decodeToString()
+
+  // STD_04
+  val FRIDA = intArrayOf(85, 49, 82, 69, 88, 122, 65, 48).decodeToString()
+
+  // STD_05
+  val SYSTEM_CALLS = intArrayOf(85, 49, 82, 69, 88, 122, 65, 49).decodeToString()
+
+  // STD_06
+  val EMULATOR = intArrayOf(85, 49, 82, 69, 88, 122, 65, 50).decodeToString()
+
+  // STD_07
+  val NONE = intArrayOf(85, 49, 82, 69, 88, 122, 65, 51).decodeToString()
+}
+
 object Obfuscation {
-  object Frida {
-    // Frida server detected
-    val title = intArrayOf(82,110,74,112,90,71,69,103,99,50,86,121,100,109,86,121,73,71,82,108,100,71,86,106,100,71,86,107)
-    // It has the potential to dynamically analyze and manipulate the behaviour of the app. This application is not supported.
-    val message = intArrayOf(83,88,81,103,97,71,70,122,73,72,82,111,90,83,66,119,98,51,82,108,98,110,82,112,89,87,119,103,100,71,56,103,90,72,108,117,89,87,49,112,89,50,70,115,98,72,107,103,89,87,53,104,98,72,108,54,90,83,66,104,98,109,81,103,98,87,70,117,97,88,66,49,98,71,70,48,90,83,66,48,97,71,85,103,89,109,86,111,89,88,90,112,98,51,86,121,73,71,57,109,73,72,82,111,90,83,66,104,99,72,65,117,73,70,82,111,97,88,77,103,89,88,66,119,98,71,108,106,89,88,82,112,98,50,52,103,97,88,77,103,98,109,57,48,73,72,78,49,99,72,66,118,99,110,82,108,90,67,52,61)
-    // Frida
-    val type = intArrayOf(82, 110, 74, 112, 90, 71, 69, 61)
-  }
-  object SystemCalls {
-    // System calls detected
-    val title = intArrayOf(85,51,108,122,100,71,86,116,73,71,78,104,98,71,120,122,73,71,82,108,100,71,86,106,100,71,86,107)
-    // It has the potential to inspect and manipulate the state of the app. This application is not supported.
-    val message = intArrayOf(83,88,81,103,97,71,70,122,73,72,82,111,90,83,66,119,98,51,82,108,98,110,82,112,89,87,119,103,100,71,56,103,97,87,53,122,99,71,86,106,100,67,66,104,98,109,81,103,98,87,70,117,97,88,66,49,98,71,70,48,90,83,66,48,97,71,85,103,99,51,82,104,100,71,85,103,98,50,89,103,100,71,104,108,73,71,70,119,99,67,52,103,86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,117,98,51,81,103,99,51,86,119,99,71,57,121,100,71,86,107,76,103,61,61)
-    // SystemCalls
-    val type = intArrayOf(85, 51, 108, 122, 100, 71, 86, 116, 81, 50, 70, 115, 98, 72, 77, 61)
-  }
-  object Root {
-    // Device is rooted
-    val title = intArrayOf(82,71,86,50,97,87,78,108,73,71,108,122,73,72,74,118,98,51,82,108,90,65,61,61)
-    // This application is not supported on rooted device.
-    val message = intArrayOf(86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,117,98,51,81,103,99,51,86,119,99,71,57,121,100,71,86,107,73,71,57,117,73,72,74,118,98,51,82,108,90,67,66,107,90,88,90,112,89,50,85,117)
-    // Root
-    val type = intArrayOf(85, 109, 57, 118, 100, 65, 61, 61)
-  }
   object DeveloperOptions {
     // USB Debugger/developer Option is ON
-    val title = intArrayOf(86,86,78,67,73,69,82,108,89,110,86,110,90,50,86,121,76,50,82,108,100,109,86,115,98,51,66,108,99,105,66,80,99,72,82,112,98,50,52,103,97,88,77,103,84,48,52,61)
+    val title = intArrayOf(86,86,78,67,73,69,82,108,89,110,86,110,90,50,86,121,76,50,82,108,100,109,86,115,98,51,66,108,99,105,66,80,99,72,82,112,98,50,52,103,97,88,77,103,84,48,52,61).decodeToString()
     // This application is not supported on USB debugging enabled device.
-    val message = intArrayOf(86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,117,98,51,81,103,99,51,86,119,99,71,57,121,100,71,86,107,73,71,57,117,73,70,86,84,81,105,66,107,90,87,74,49,90,50,100,112,98,109,99,103,90,87,53,104,89,109,120,108,90,67,66,107,90,88,90,112,89,50,85,117)
-    // DeveloperOptions
-    val type = intArrayOf(82, 71, 86, 50, 90, 87, 120, 118, 99, 71, 86, 121, 84, 51, 66, 48, 97, 87, 57, 117, 99, 119, 61, 61)
+    val message = intArrayOf(86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,117,98,51,81,103,99,51,86,119,99,71,57,121,100,71,86,107,73,71,57,117,73,70,86,84,81,105,66,107,90,87,74,49,90,50,100,112,98,109,99,103,90,87,53,104,89,109,120,108,90,67,66,107,90,88,90,112,89,50,85,117).decodeToString()
+   }
+
+  object GenericErrorInfo {
+    // Error Code -
+    val title = intArrayOf(82, 88, 74, 121, 98, 51, 73, 103, 81, 50, 57, 107, 90, 83, 65, 116, 73, 65, 61, 61).decodeToString()
+
+    // App or device not supported
+    val message = intArrayOf(81, 88, 66, 119, 73, 71, 57, 121, 73, 71, 82, 108, 100, 109, 108, 106, 90, 83, 66, 117, 98, 51, 81, 103, 99, 51, 86, 119, 99, 71, 57, 121, 100, 71, 86, 107).decodeToString()
   }
-  object Emulator {
-    // Emulator detected
-    val title = intArrayOf(82,87,49,49,98,71,70,48,98,51,73,103,90,71,86,48,90,87,78,48,90,87,81,61)
-    // This application is not supported on emulator.
-    val message = intArrayOf(86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,117,98,51,81,103,99,51,86,119,99,71,57,121,100,71,86,107,73,71,57,117,73,71,86,116,100,87,120,104,100,71,57,121,76,103,61,61)
-    // Emulator
-    val type = intArrayOf(82, 87, 49, 49, 98, 71, 70, 48, 98, 51, 73, 61)
-  }
-  object AppIntegrity {
-    // App is not secure
-    val title = intArrayOf(81,88,66,119,73,71,108,122,73,71,53,118,100,67,66,122,90,87,78,49,99,109,85,61)
-    // Please uninstall the application and install a secure version from the play store.
-    val message = intArrayOf(85,71,120,108,89,88,78,108,73,72,86,117,97,87,53,122,100,71,70,115,98,67,66,48,97,71,85,103,89,88,66,119,98,71,108,106,89,88,82,112,98,50,52,103,89,87,53,107,73,71,108,117,99,51,82,104,98,71,119,103,89,83,66,122,90,87,78,49,99,109,85,103,100,109,86,121,99,50,108,118,98,105,66,109,99,109,57,116,73,72,82,111,90,83,66,119,98,71,70,53,73,72,78,48,98,51,74,108,76,103,61,61)
-    // AppIntegrity
-    val type = intArrayOf(81, 88, 66, 119, 83, 87, 53, 48, 90, 87, 100, 121, 97, 88, 82, 53)
-  }
+
   object None {
     // No security issues detected
-    val title = intArrayOf(84,109,56,103,99,50,86,106,100,88,74,112,100,72,107,103,97,88,78,122,100,87,86,122,73,71,82,108,100,71,86,106,100,71,86,107)
+    val title = intArrayOf(84,109,56,103,99,50,86,106,100,88,74,112,100,72,107,103,97,88,78,122,100,87,86,122,73,71,82,108,100,71,86,106,100,71,86,107).decodeToString()
     // This application is safe to use.
-    val message = intArrayOf(86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,122,89,87,90,108,73,72,82,118,73,72,86,122,90,83,52,61)
-    // None
-    val type = intArrayOf(84, 109, 57, 117, 90, 81, 61, 61)
-  }
+    val message = intArrayOf(86,71,104,112,99,121,66,104,99,72,66,115,97,87,78,104,100,71,108,118,98,105,66,112,99,121,66,122,89,87,90,108,73,72,82,118,73,72,86,122,90,83,52,61).decodeToString()
+   }
 }
 
 
@@ -87,86 +75,59 @@ object SecurityService {
   var result: String? = null
 
   private fun getSecurityType(context: Context): String {
-//    if (AppIntegrity.isValid(context).not()) {
-//      return SecurityType.AppIntegrity
-//    }
-    if (DeveloperOptions.isDetected(context)) {
-      return Obfuscation.DeveloperOptions.type.decodeToString()
-    }
-    if (Root.isDetected(context)) {
-      return Obfuscation.Root.type.decodeToString()
-    }
-    if (Frida.isDetected()) {
-      return Obfuscation.Frida.type.decodeToString()
-    }
-    if (SystemCalls.isDetected()) {
-      return Obfuscation.SystemCalls.type.decodeToString()
-    }
+    val isDeveloperOptionsDetected = DeveloperOptions.isDetected(context)
+    val isAppIntegrityNotValid = AppIntegrity.isValid(context).not()
+    val isRootDetected = Root.isDetected(context)
+    val isFridaDetected = Frida.isDetected()
+    val isSystemCallsDetected = SystemCalls.isDetected()
+    val isEmulatorDetected = Emulator.isDetected(context)
 
-    if (Emulator.isDetected(context)) {
-      return Obfuscation.Emulator.type.decodeToString()
+    if (isDeveloperOptionsDetected) {
+      return SecurityType.DEVELOPER_OPTIONS
     }
-    return Obfuscation.None.type.decodeToString()
+    if (isAppIntegrityNotValid) {
+      return SecurityType.APP_INTEGRITY
+    }
+    if (isRootDetected) {
+      return SecurityType.ROOT
+    }
+    if (isFridaDetected) {
+      return SecurityType.FRIDA
+    }
+    if (isSystemCallsDetected) {
+      return SecurityType.SYSTEM_CALLS
+    }
+    if (isEmulatorDetected) {
+      return SecurityType.EMULATOR
+    }
+    return SecurityType.NONE
   }
 
   private fun getTitleAndMessageForSecurityType(type: String): Triple<String, String, Boolean> {
     println(
-      "${SECURITY_LOG_TAG.decodeToString()} - getTitleAndMessageForSecurityType \n" +
-        "Frida - ${Obfuscation.Frida.type.decodeToString()}, ${Obfuscation.Frida.title.decodeToString()}, ${Obfuscation.Frida.message.decodeToString()}\n" +
-        "SystemCalls - ${Obfuscation.SystemCalls.type.decodeToString()}, ${Obfuscation.SystemCalls.title.decodeToString()}, ${Obfuscation.SystemCalls.message.decodeToString()}\n" +
-        "Root - ${Obfuscation.Root.type.decodeToString()}, ${Obfuscation.Root.title.decodeToString()}, ${Obfuscation.Root.message.decodeToString()}\n" +
-        "DeveloperOptions - ${Obfuscation.DeveloperOptions.type.decodeToString()}, ${Obfuscation.DeveloperOptions.title.decodeToString()}, ${Obfuscation.DeveloperOptions.message.decodeToString()}\n" +
-        "Emulator - ${Obfuscation.Emulator.type.decodeToString()}, ${Obfuscation.Emulator.title.decodeToString()}, ${Obfuscation.Emulator.message.decodeToString()}\n" +
-        "AppIntegrity - ${Obfuscation.AppIntegrity.type.decodeToString()}, ${Obfuscation.AppIntegrity.title.decodeToString()}, ${Obfuscation.AppIntegrity.message.decodeToString()}\n" +
-        "None - ${Obfuscation.None.type.decodeToString()} ${Obfuscation.None.title.decodeToString()}, ${Obfuscation.None.message.decodeToString()}"
+      "${SECURITY_LOG_TAG.decodeToString()} - getTitleAndMessageForSecurityType - type: $type"
     )
     return when (type) {
-      Obfuscation.Frida.type.decodeToString() -> Triple(
-        Obfuscation.Frida.title.decodeToString(),
-        Obfuscation.Frida.message.decodeToString(),
-        false
-      )
-
-      Obfuscation.SystemCalls.type.decodeToString() -> Triple(
-        Obfuscation.SystemCalls.title.decodeToString(),
-        Obfuscation.SystemCalls.message.decodeToString(),
-        false
-      )
-
-      Obfuscation.Root.type.decodeToString() -> Triple(
-        Obfuscation.Root.title.decodeToString(),
-        Obfuscation.Root.message.decodeToString(),
-        false
-      )
-
-      Obfuscation.DeveloperOptions.type.decodeToString() -> Triple(
-        Obfuscation.DeveloperOptions.title.decodeToString(),
-        Obfuscation.DeveloperOptions.message.decodeToString(),
+      SecurityType.DEVELOPER_OPTIONS -> Triple(
+        Obfuscation.DeveloperOptions.title,
+        Obfuscation.DeveloperOptions.message,
         true
       )
 
-      Obfuscation.Emulator.type.decodeToString() -> Triple(
-        Obfuscation.Emulator.title.decodeToString(),
-        Obfuscation.Emulator.message.decodeToString(),
-        false
-      )
-
-      Obfuscation.AppIntegrity.type.decodeToString() -> Triple(
-        Obfuscation.AppIntegrity.title.decodeToString(),
-        Obfuscation.AppIntegrity.message.decodeToString(),
-        false
-      )
-
-      Obfuscation.None.type.decodeToString() -> Triple(
-        Obfuscation.None.title.decodeToString(),
-        Obfuscation.None.message.decodeToString(),
+      SecurityType.APP_INTEGRITY,
+      SecurityType.FRIDA,
+      SecurityType.ROOT,
+      SecurityType.SYSTEM_CALLS,
+      SecurityType.EMULATOR -> Triple(
+        "${Obfuscation.GenericErrorInfo.title} $type",
+        Obfuscation.GenericErrorInfo.message,
         false
       )
 
       else -> {
         Triple(
-          Obfuscation.None.title.decodeToString(),
-          Obfuscation.None.message.decodeToString(),
+          Obfuscation.None.title,
+          Obfuscation.None.message,
           false
         )
       }
@@ -197,7 +158,7 @@ object SecurityService {
 
   private fun dispatchResult(callback: (title: String, message: String) -> Unit) {
     result?.let {
-      if (it.equals(Obfuscation.None.type.decodeToString())) {
+      if (it == SecurityType.NONE) {
         callback("", "")
         return
       }
@@ -251,7 +212,7 @@ object SecurityService {
       result = type
       dispatchResult(callback)
       notifyQueue()
-      if (!type.equals(Obfuscation.None.type.decodeToString())) {
+      if (type != SecurityType.NONE) {
         launchSecurityActivity(activityContext, type)
       }
       delay(3000)
